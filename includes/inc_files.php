86f80a5da2cdf92283113ff80e48a0fd4b266e7e
root <root@mediaserver.Belkin>
2010-02-18 17:59:35 +0100

    added missing ram folder & changes in inc_files.php Committer: Alib <aliboba@free.fr> 	new file:   includes/inc_files.php 	new file:   ram/dummy.txt

---
+++ includes/inc_files.php
@@ -0,0 +1,36 @@
+<?php
+
+function mediagetinfostream($stream = "")
+{
+
+	global $mediainfopath, $ffmpegpath;
+
+	// Get info
+	exec($mediainfopath ." \"" .$stream ."\"", $mediainfo);
+
+	$info = "";
+	$title = "";
+
+	// Parse info
+	$count = count($mediainfo);
+	for ($i = 0; $i < $count; $i++)
+	{
+		if (!strncmp($mediainfo[$i], "Video", strlen("Video")) || !strncmp($mediainfo[$i], "Audio", strlen("Audio")))
+			break;
+
+		if (!strncmp($mediainfo[$i], "Format", strlen("Format")))
+			$title = substr(strstr($mediainfo[$i], ": "), 2);
+		else if (!strncmp($mediainfo[$i], "Format/Info", strlen("Format/Info")))
+			$title = substr(strstr($mediainfo[$i], ": "), 2);
+		else if (!strncmp($mediainfo[$i], "Duration", strlen("Duration")))
+			$info = substr(strstr($mediainfo[$i], ": "), 2);
+	}
+
+	// Extract a thumbnail
+	exec("rm ram/stream-tb.png");
+	exec($ffmpegpath ." -y -i \"" .$stream ."\" -an -ss 00:00:05.00 -r 1 -vframes 1 -s 80x80 -f mjpeg ram/stream-tb.png");
+	
+	return array($title, $info);
+}
+
+
