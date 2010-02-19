c109894e17e5d77b027774e9fb1e236bcc1b2bfd
Alib <aliboba@free.fr>
2010-02-19 17:23:27 +0100

    adding pictos, nologos*, and preparing some stuff for timers & epg

---
+++ includes/inc_epg.php
@@ -0,0 +1,15 @@
+<?php
+print "<body onorientationchange=\"updateOrientation();\" onload=\"updateOrientation();\">\r\n";
+print "<div id=\"topbar\" class=\"transparent\">\r\n";
+print "<div id=\"leftnav\">\r\n";
+print "<a href=\"index.php\"><img alt=\"home\" src=\"images/home.png\" /></a></div>\r\n";
+print "<div id=\"title\">iStreamdev</div>\r\n";
+print "</div>\r\n";
+print "<div id=\"content\">\r\n";
+print " <span class=\"graytitle\">Program Guide</span>\r\n";
+print " <ul class=\"pageitem\">";
+print " <li class=\"textbox\"><span class=\"header\">Incoming feature</span";
+print " <p>To do...</p></li>";
+print "</div>\r\n";
+?>
+
