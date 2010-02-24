b28b92b318e55387fa24283195163bab32c8bf69
Alib <aliboba@free.fr>
2010-02-24 20:49:04 +0100

    added manifest file

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include('includes/include.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" manifest="istreamdev.manifest">
 
 <head>
 <meta content="yes" name="apple-mobile-web-app-capable" />
