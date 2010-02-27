e3111d568164aa1f0d3bd303e3b80dcb6f375047
TheTroll <trolldev@gmail.com>
2010-02-27 19:55:26 +0100

    Fixed status

--- streamstatus.php
+++ streamstatus.php
@@ -12,14 +12,22 @@ echo "<?xml version=\"1.0\"?>\n";
 
 echo "<status>\n";
 
+$session = $_REQUEST['session'];
+
+exec('echo ' .$session .' >/tmp/aa');
+
 // First check that we are allowed to create a new encoding process
-$nbencprocess = exec("find ram/ -name segmenter.pid | wc | awk '{ print $1 }'");
-if ($nbencprocess >= $maxencodingprocesses)
-	echo "<streamstatus>error</streamstatus><message>Error: maximun number of sessions reached</message>\n";
+if (($session == "") || !count(glob('ram/' .$session)))
+{
+	$nbencprocess = exec("find ram/ -name segmenter.pid | wc | awk '{ print $1 }'");
+
+	if ($nbencprocess >= $maxencodingprocesses)
+		echo "<streamstatus>error</streamstatus><message>Error: maximun number of sessions reached</message>\n";
+	else
+		echo "<streamstatus>error</streamstatus><message>Error: cannot create session</message>\n";
+}
 else
 {
-	$session = $_REQUEST['session'];
-
 	$cnt = 0;
 	while ( ( count(glob('ram/' .$session . '/*.ts')) < 2 ) && ( $cnt < 25 ) )
 	{
