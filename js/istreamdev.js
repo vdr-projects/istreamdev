1290a860a86254c95c052ec3a207c82cceaca81f
Alib <aliboba@free.fr>
2010-03-17 15:52:56 +0100

    update js

--- js/istreamdev.js
+++ js/istreamdev.js
@@ -213,10 +213,7 @@ function gen_channels(category) {
 			$.each(data.channel,function(i,channel){
 				$("#chan_menu").append('<li class="channellist"><a class="chan_but" href="#"><img src="logos/' + channel.name + '.png"/><small class="counter">' + channel.number + '</small><span class="name">' + channel.name + '</span><span class="comment">' + channel.now_title + '</span></a></li>');
 				});
-			$("#chan_menu li:last a img").load(function () {
-
 			json_complete('#channels','cube');
-			});
 		})
 }
 
