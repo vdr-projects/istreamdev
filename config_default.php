405e5e51850623b46fadfbd09837cf218ff33de5
TheTroll <trolldev@gmail.com>
2010-03-18 17:54:04 +0100

    Cleanup

--- config_default.php
+++ config_default.php
@@ -5,33 +5,25 @@
 	$httppath = '/istreamdev/';	// Absolute path to the index.php file. 					//Don't put http://yourdomain !!
 
 	// VDR configuration
-
-	$vdrenabled=1;	// enable/disable VDR features
-	$vdrchannels='/etc/vdr/channels.conf';	// VDR channel list
+	$vdrenabled=1;						// enable/disable VDR features
+	$vdrchannels='/etc/vdr/channels.conf';			// VDR channel list
 	$svdrpport=2001;					// SVDRP port
 	$svdrpip='127.0.0.1';					// SVDRP ip
 	$vdrstreamdev='http://127.0.0.1:3000/TS/';		// VDR streamdev URL
-	$vdrrecpath='/video/';	//VDR recording directory
+	$vdrrecpath='/video/';					//VDR recording directory
 
 	// Media configuration
 	$videotypes='avi mkv ts mov mp4 wmv flv mpg mpeg mpeg2 mpv ';	// Supported video extensions (must finish with a space)
 	$audiotypes='mp3 aac wav ';					// Supported audio extensions
-
-        //                      1:vid 2:aud     Source name     Source path
-	$mediasource=array();
-	$mediasources[]=array ( 1,              'Video',        '/mnt/media/movies/');
-	$mediasources[]=array ( 2,              'Audio',        '/mnt/media/music/');
-
-	// TEMPORARY
-	$videosource = '/mnt/media/movies/';
-	$audiosource = '/mnt/media/music/';
+	$videosource = '/mnt/media/movies/';				// Video files directory
+	$audiosource = '/mnt/media/music/';				// Audio files directory
 
 	// Encoding (The name cannot be changed)
 	//			Name		Video	Audio	Audio channels	Resolution
 	$quality=array (	'edge'	=>	'128k	64k	1		240x160',
 				'3g'	=>	'350k	64k	1		408x272',
 				'wifi'	=>	'512k	128k	2		480x320');
-	$maxencodingprocesses=10;                               // Max simultaneous encoding processes
+	$maxencodingprocesses=10;		// Max simultaneous encoding processes
 
 	// Misc
 	$ffmpegpath = '/usr/bin/ffmpeg';	//path to ffmpeg binary
