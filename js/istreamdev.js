a5234303ee152e0f24d626425809d918b10d9b1f
Alib <aliboba@free.fr>
2010-03-18 11:07:37 +0100

    fix type

--- js/istreamdev.js
+++ js/istreamdev.js
@@ -184,9 +184,9 @@ var dataString = "action=getRunningSessions";
 			session = broadcast.session;
 			name = broadcast.name;
 			type = broadcast.type;
-			if (type == 'tv') { var pic='tv.png';var typerunning = 'TV:' }
-			else if (type == 'rec') { var pic='record.png'; var typerunning = 'REC:' }
-			else if (type == 'vid') { var pic='video.png'; var typerunning = 'VID:' }
+			if (type == 'tv') { var pic='tv.png';var typerunning = 'Tv: ' }
+			else if (type == 'rec') { var pic='record.png'; var typerunning = 'Rec: ' }
+			else if (type == 'vid') { var pic='video.png'; var typerunning = 'Vid: ' }
 			$('#home #runningsessions').append('<li class="arrow"><a rel="' + session + '" href="#"><img class="menuicon" src="img/' + pic + '" /><span class="menuname">' + typerunning + name + '</span></a></li>');
 			});
 			$('#home #runningsessions').append('<li><a rel="killsessions" href="#"><span class="menuname">Stop all sessions</span></a></li>');
