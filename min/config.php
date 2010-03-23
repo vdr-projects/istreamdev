ce0fc110e6285d56200c9dc25375ebcc32ded8bd
Alib <aliboba@free.fr>
2010-03-23 11:36:08 +0100

    added missing file in git

--- min/config.php
+++ min/config.php
@@ -12,7 +12,7 @@
  * To allow debugging, set this option to true and add "&debug=1" to 
  * a URI. E.g. /min/?f=script1.js,script2.js&debug=1
  */
-$min_allowDebugFlag = false;
+$min_allowDebugFlag = true;
 
 
 /**
@@ -25,7 +25,7 @@ $min_allowDebugFlag = false;
  *
  * @todo cache system does not have error logging yet.
  */
-$min_errorLogger = false;
+$min_errorLogger = true;
 
 
 /**
